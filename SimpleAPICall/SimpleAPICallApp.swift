2f63d195f560778f65a6786f799df618f71f036d
Nicolò Curioni <102799475+nicolocurioni96@users.noreply.github.com>
2025-11-29 18:05:18 +0100

    Initial Commit

---
+++ SimpleAPICall/SimpleAPICallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAPICallApp.swift
+//  SimpleAPICall
+//
+//  Created by Nicolò Curioni on 29/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAPICallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
